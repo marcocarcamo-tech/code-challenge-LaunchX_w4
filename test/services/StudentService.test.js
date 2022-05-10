079d8f2cd79daa91d8cb4d010e11d2feffabe799
Marco <macdev.gmail@gmail.com>
2022-05-09 23:42:39 -0500

    Test for StudenService class method getStudentsInfo added and make it fail on purpose

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,37 @@
+const StundentService = require("../../lib/services/StudentService");
+
+describe("Unit test for ExplorerService", ()=>{
+    test("1. Get all students listed with all their fields", ()=>{
+        const students = [
+            {
+                "id": "6264d5d89f1df827eb84bb23",
+                "name": "Warren", 
+              },
+              {
+                "id": "6264d5d85cf81c496446b67f",
+                "name": "Lucinda",
+              },
+              {
+                "id": "6264d5d8cda17de0d2e9f236",
+                "name": "Fuentes",
+              }
+        ];
+    
+        const studentsList = StudentService.getStudentsInfo();
+    
+        expect(studentsList).toMatch([
+            {
+                "id": "6264d5d89f1df827eb84bb23",
+                "name": "Warren", 
+              },
+              {
+                "id": "6264d5d85cf81c496446b67f",
+                "name": "Lucinda",
+              },
+              {
+                "id": "6264d5d8cda17de0d2e9f236",
+                "name": "Fuentes",
+              }
+        ]);
+    });
+});
