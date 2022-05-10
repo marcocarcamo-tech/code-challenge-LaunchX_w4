b2e6403b8e51cd3e63cfcb8a012ccf37dbfe7d42
Marco <macdev.gmail@gmail.com>
2022-05-10 00:37:52 -0500

    ExplorerService class added and method getStudentsInfo and all tests passed

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,36 +1,36 @@
-const StundentService = require("../../lib/services/StudentService");
+const StudentService = require("../../lib/services/StudentService");
 
 describe("Unit test for ExplorerService", ()=>{
     test("1. Get all students listed with all their fields", ()=>{
         const students = [
             {
-                "id": "6264d5d89f1df827eb84bb23",
-                "name": "Warren", 
+                id: "6264d5d89f1df827eb84bb23",
+                name: "Warren", 
               },
               {
-                "id": "6264d5d85cf81c496446b67f",
-                "name": "Lucinda",
+                id: "6264d5d85cf81c496446b67f",
+                name: "Lucinda",
               },
               {
-                "id": "6264d5d8cda17de0d2e9f236",
-                "name": "Fuentes",
+                id: "6264d5d8cda17de0d2e9f236",
+                name: "Fuentes",
               }
         ];
     
-        const studentsList = StudentService.getStudentsInfo();
+        const studentsList = StudentService.getStudentsInfo(students);
     
-        expect(studentsList).toMatch([
+        expect(studentsList).toMatchObject([
             {
-                "id": "6264d5d89f1df827eb84bb23",
-                "name": "Warren", 
+                id: "6264d5d89f1df827eb84bb23",
+                name: "Warren", 
               },
               {
-                "id": "6264d5d85cf81c496446b67f",
-                "name": "Lucinda",
+                id: "6264d5d85cf81c496446b67f",
+                name: "Lucinda",
               },
               {
-                "id": "6264d5d8cda17de0d2e9f236",
-                "name": "Fuentes",
+                id: "6264d5d8cda17de0d2e9f236",
+                name: "Fuentes",
               }
         ]);
     });
