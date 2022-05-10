fb71338a293490d9c7fbc8da0522a8ad67b56c85
Marco <macdev.gmail@gmail.com>
2022-05-09 23:30:21 -0500

    Reader class added and all tests passed

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,11 +2,11 @@ const Reader = require("../../lib/utils/Reader");
 
 describe("Unit test for Reader", ()=>{
     test("1. Check not null data", ()=>{
-        const data = Reader.readJsonFile("explorers.json");
+        const data = Reader.readJsonFile("visualpartners.json");
         expect(data).not.toBe(null);
     });
     test("2. Returns an object", ()=>{
-        const data = Reader.readJsonFile("explorers.json");
+        const data = Reader.readJsonFile("visualpartners.json");
         expect(typeof data).toBe("object");
     });
 });
