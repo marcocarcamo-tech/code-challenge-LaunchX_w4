be771201420fc25964344496d4d9742e4ace59de
Marco <macdev.gmail@gmail.com>
2022-05-10 21:15:20 -0500

    Test for StudentController Class getStudentsByHighCredits created and make it fail on purpose

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -12,5 +12,23 @@ describe("Unit test for StudentController", ()=>{
     
         expect(certifiedStudentsEmail[0]).toBe("Todd@visualpartnership.xyz");
     });
+    test("3. Get students by credits validation", ()=>{
+        
+        const studentsHighCredits = StudentController.getStudentsByHighCredits();
+    
+        expect(studentsHighCredits[0]).toMatchObject({
+            id: "6264d5d89f1df827eb84bb23",
+            name: "Warren",
+            email: "Todd@visualpartnership.xyz",
+            credits: 508,
+            enrollments: [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Avanzado"
+            ],
+            previousCourses: 1,
+            haveCertification: true
+          });
+    });
+
     
 });
